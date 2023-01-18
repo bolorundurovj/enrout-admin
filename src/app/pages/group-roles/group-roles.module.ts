9ae44429664eff82f23d17567f0b13b2a95004e2
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-18 17:25:37 +0100

    feat: Add Group Roles Module

---
+++ src/app/pages/group-roles/group-roles.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GroupRolesRoutingModule } from './group-roles-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GroupRolesRoutingModule
+  ]
+})
+export class GroupRolesModule { }
