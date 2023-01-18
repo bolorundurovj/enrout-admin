da3901b58f48a62718a2c5d70588e0d643ce01aa
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-18 18:12:18 +0100

    feat: Group Listing

--- src/app/pages/groups/groups.module.ts
+++ src/app/pages/groups/groups.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GroupsRoutingModule } from './groups-routing.module';
+import { GroupListComponent } from './group-list/group-list.component';
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GroupListComponent
+  ],
   imports: [
     CommonModule,
-    GroupsRoutingModule
+    GroupsRoutingModule,
+    NzTableModule,
+    NzButtonModule
   ]
 })
 export class GroupsModule { }
