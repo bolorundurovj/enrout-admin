444fc5bfb9538a46c0fe8b487ed4934dfb6bd516
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-19 01:41:40 +0100

    feat: Setup Toast Service

---
+++ src/app/lib/services/toast.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+export class ToastMessage {
+  key: string = (Math.random() * 10000).toString();
+  content: string;
+  style: "success" | "info" | "warning" | "error";
+  time: Number = new Date().getTime()
+
+  constructor(content: string, style?: "success" | "info" | "warning" | "error") {
+    this.content = content;
+    this.style = style || 'info';
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  messages$: BehaviorSubject<Array<ToastMessage>> = new BehaviorSubject<Array<ToastMessage>>([])
+
+  constructor() {
+  }
+
+  getMessages() {
+    return this.messages$.asObservable();
+  }
+
+  sendMessage(content: string, style: "success" | "info" | "warning" | "error") {
+    const message = new ToastMessage(content, style);
+    this.messages$.next([...this.messages$.value, message])
+  }
+
+  dismissMessage(messageKey: string) {
+    const arr = this.messages$.value.filter((x) => x.key !== messageKey)
+    this.messages$.next(arr)
+  }
+}
