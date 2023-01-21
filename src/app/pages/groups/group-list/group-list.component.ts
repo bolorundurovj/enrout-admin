c132945e7b3b2e35206b23a3375438543e7ec2d0
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-21 18:12:51 +0100

    feat: Get Groups from API

--- src/app/pages/groups/group-list/group-list.component.ts
+++ src/app/pages/groups/group-list/group-list.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ToastService} from "../../../lib/services/toast.service";
+import {PaginationParams} from "../../../lib/classes/pagination-params";
+import {GroupService} from "../../../lib/services/group/group.service";
+import {IGroup} from "../../../lib/interfaces/igroup";
+import {IPaginatedMetadata} from "../../../lib/interfaces";
+import {LoaderService} from "../../../lib/services/loader/loader.service";
 
 export interface Data {
   id: number;
@@ -20,17 +25,26 @@ export class GroupListComponent implements OnInit {
   checked = false;
   loading = false;
   indeterminate = false;
-  listOfData: readonly Data[] = [];
-  listOfCurrentPageData: readonly Data[] = [];
-  setOfCheckedId = new Set<number>();
+  listOfData: readonly IGroup[] = [];
+  listOfCurrentPageData: readonly IGroup[] = [];
+  setOfCheckedId = new Set<string>();
   isVisible = false;
   isOkLoading = false;
   isDeleting = false;
   validateForm!: FormGroup;
   formMode: 'new' | 'edit' = "new";
   group: any = {};
+  groupPagination = new PaginationParams()
+  paginationMeta: IPaginatedMetadata = {
+    "page": 1,
+    "take": 10,
+    "itemCount": 0,
+    "pageCount": 0,
+    "hasPreviousPage": false,
+    "hasNextPage": true
+  };
 
-  constructor(private fb: FormBuilder, private toastService: ToastService) {
+  constructor(private fb: FormBuilder, private toastService: ToastService, private groupService: GroupService, private loader: LoaderService) {
   }
 
   showModal(): void {
@@ -85,58 +99,81 @@ export class GroupListComponent implements OnInit {
     this.isVisible = false;
   }
 
-  updateCheckedSet(id: number, checked: boolean): void {
-    if (checked) {
-      this.setOfCheckedId.add(id);
-    } else {
-      this.setOfCheckedId.delete(id);
-    }
+  pageChange(page: number){
+    this.groupPagination.page = page;
+    this.getGroups();
   }
 
-  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
-    this.listOfCurrentPageData = listOfCurrentPageData;
-    this.refreshCheckedStatus();
+  limitChange(limit: number){
+    this.groupPagination.take = limit;
+    this.getGroups();
   }
 
-  refreshCheckedStatus(): void {
-    const listOfEnabledData = this.listOfCurrentPageData.filter(({disabled}) => !disabled);
-    this.checked = listOfEnabledData.every(({id}) => this.setOfCheckedId.has(id));
-    this.indeterminate = listOfEnabledData.some(({id}) => this.setOfCheckedId.has(id)) && !this.checked;
-  }
-
-  onItemChecked(id: number, checked: boolean): void {
-    this.updateCheckedSet(id, checked);
-    this.refreshCheckedStatus();
-  }
-
-  onAllChecked(checked: boolean): void {
-    this.listOfCurrentPageData
-      .filter(({disabled}) => !disabled)
-      .forEach(({id}) => this.updateCheckedSet(id, checked));
-    this.refreshCheckedStatus();
-  }
-
-  sendRequest(): void {
+  getGroups() {
     this.loading = true;
-    const requestData = this.listOfData.filter(data => this.setOfCheckedId.has(data.id));
-    console.log(requestData);
-    setTimeout(() => {
-      this.setOfCheckedId.clear();
-      this.refreshCheckedStatus();
-      this.loading = false;
-    }, 1000);
+    this.groupService.retrieveGroups(this.groupPagination)
+      .subscribe(
+        (response) => {
+          if (response) {
+            this.listOfData = response.data;
+            this.paginationMeta = response.meta;
+          } else {
+            this.toastService.error("An error occurred, please try again")
+          }
+        },
+        (error) => {
+          console.error(error)
+          this.toastService.error(`${error.error?.error || 'An error occurred'}`);
+        },
+        () => {
+          this.loading = false;
+        }
+      );
   }
+
+  // updateCheckedSet(id: number, checked: boolean): void {
+  //   if (checked) {
+  //     this.setOfCheckedId.add(id);
+  //   } else {
+  //     this.setOfCheckedId.delete(id);
+  //   }
+  // }
+
+
+  // refreshCheckedStatus(): void {
+  //   const listOfEnabledData = this.listOfCurrentPageData.filter(({disabled}) => !disabled);
+  //   this.checked = listOfEnabledData.every(({id}) => this.setOfCheckedId.has(id));
+  //   this.indeterminate = listOfEnabledData.some(({id}) => this.setOfCheckedId.has(id)) && !this.checked;
+  // }
+
+  // onItemChecked(id: number, checked: boolean): void {
+  //   this.updateCheckedSet(id, checked);
+  //   this.refreshCheckedStatus();
+  // }
+
+  // onAllChecked(checked: boolean): void {
+  //   this.listOfCurrentPageData
+  //     .filter(({disabled}) => !disabled)
+  //     .forEach(({id}) => this.updateCheckedSet(id, checked));
+  //   this.refreshCheckedStatus();
+  // }
+
+  // sendRequest(): void {
+  //   this.loading = true;
+  //   const requestData = this.listOfData.filter(data => this.setOfCheckedId.has(data.id));
+  //   console.log(requestData);
+  //   setTimeout(() => {
+  //     this.setOfCheckedId.clear();
+  //     this.refreshCheckedStatus();
+  //     this.loading = false;
+  //   }, 1000);
+  // }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       name: [null, [Validators.required]],
     });
-    this.listOfData = new Array(100).fill(0).map((_, index) => ({
-      id: index,
-      name: `Group ${index}`,
-      age: 32,
-      address: `London, Park Lane no. ${index}`,
-      disabled: index % 2 === 0
-    }));
+
+    this.getGroups()
   }
 }
