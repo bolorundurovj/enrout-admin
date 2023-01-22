fa8364249233028354c8179bceab6a760a4900cf
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-22 09:05:45 +0100

    feat(group-roles): Setup Group Roles Routing

--- src/app/pages/group-roles/group-roles-routing.module.ts
+++ src/app/pages/group-roles/group-roles-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GroupRoleListComponent} from "./group-role-list/group-role-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: GroupRoleListComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GroupRolesRoutingModule { }
+export class GroupRolesRoutingModule {
+}
--- src/app/pages/group-roles/group-roles.module.ts
+++ src/app/pages/group-roles/group-roles.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GroupRolesRoutingModule } from './group-roles-routing.module';
+import { GroupRoleListComponent } from './group-role-list/group-role-list.component';
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NzModalModule} from "ng-zorro-antd/modal";
+import {IconsProviderModule} from "../../icons-provider.module";
+import {NzDividerModule} from "ng-zorro-antd/divider";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GroupRoleListComponent
+  ],
   imports: [
     CommonModule,
-    GroupRolesRoutingModule
+    GroupRolesRoutingModule,
+    NzSelectModule,
+    NzTableModule,
+    NzButtonModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzModalModule,
+    IconsProviderModule,
+    NzDividerModule
   ]
 })
 export class GroupRolesModule { }
