1d295e361076b6cc1023ef77e7042fd7703b2a8a
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-28 12:21:38 +0100

    feat(workflow): Setup Workflow Routes

--- src/app/pages/workflows/workflows-routing.module.ts
+++ src/app/pages/workflows/workflows-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WorkflowListComponent} from "./workflow-list/workflow-list.component";
+import {WorkflowDetailComponent} from "./workflow-detail/workflow-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WorkflowListComponent},
+  {
+    path: ':id',
+    component: WorkflowDetailComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class WorkflowsRoutingModule { }
+export class WorkflowsRoutingModule {
+}
